3afff7d0a1aea6e616d348768d2cd9ff04424406
Jason Felice <jason.m.felice@gmail.com>
2011-12-31 19:59:06 -0500

    Fix up RSpecs for Paths module

--- spec/paths_spec.rb
+++ spec/paths_spec.rb
@@ -9,22 +9,68 @@ describe ZergXcode::Paths do
 
   include ZergXcode::Paths
   
-  it "should ??" do
-    project_file_at('test/fixtures/ZergSupport').should == 'test/fixtures/ZergSupport.xcodeproj/project.pbxproj'
-    project_file_at('test/fixtures/ZergSupport.xcodeproj').should == 'test/fixtures/ZergSupport.xcodeproj/project.pbxproj'
-    project_file_at('test/fixtures').should == 'test/fixtures/project.pbxproj'
-    project_file_at('test').should == 'test/fixtures/project.pbxproj'
-    project_file_at('test/fixtures/TestApp').should == 'test/fixtures/TestApp/TestApp.xcodeproj/project.pbxproj'
-    project_file_at('test/fixtures/TestApp/TestApp.xcodeproj').should == 'test/fixtures/TestApp/TestApp.xcodeproj/project.pbxproj'
+  describe '#project_file_at' do
+    context "when given 'test/fixtures/ZergSupport'" do
+      subject {project_file_at('test/fixtures/ZergSupport')}
+      it {should == 'test/fixtures/ZergSupport.xcodeproj/project.pbxproj'}
+    end
+
+    context "when given 'test/fixtures/ZergSupport.xcodeproj'" do
+      subject {project_file_at('test/fixtures/ZergSupport.xcodeproj')}
+      it {should == 'test/fixtures/ZergSupport.xcodeproj/project.pbxproj'}
+    end
+
+    context "when given 'test/fixtures'" do
+      subject {project_file_at('test/fixtures')}
+      it {should == 'test/fixtures/project.pbxproj'}
+    end
+
+    context "when given 'test'" do
+      subject {project_file_at('test')}
+      it {should == 'test/fixtures/project.pbxproj'}
+    end
+
+    context "when given 'test/fixtures/TestApp'" do
+      subject {project_file_at('test/fixtures/TestApp')}
+      it {should == 'test/fixtures/TestApp/TestApp.xcodeproj/project.pbxproj'}
+    end
+
+    context "when given 'test/fixtures/TestApp/TestApp.xcodeproj'" do
+      subject {project_file_at('test/fixtures/TestApp/TestApp.xcodeproj')}
+      it {should == 'test/fixtures/TestApp/TestApp.xcodeproj/project.pbxproj'}
+    end
   end
-  
-  it "should ??" do
-    project_root_at('test/fixtures/ZergSupport').should == 'test/fixtures'
-    project_root_at('test/fixtures/ZergSupport.xcodeproj').should == 'test/fixtures'
-    project_root_at('test/fixtures').should == 'test'
-    project_root_at('test').should == 'test'
-    project_root_at('test/fixtures/TestApp').should == 'test/fixtures/TestApp'
-    project_root_at('test/fixtures/TestApp/TestApp.xcodeproj').should == 'test/fixtures/TestApp'
+
+  describe '#project_root_at' do
+    context "when given 'test/fixtures/ZergSupport'" do
+      subject {project_root_at('test/fixtures/ZergSupport')}
+      it {should == 'test/fixtures'}
+    end
+
+    context "when given 'test/fixtures/ZergSupport.xcodeproj'" do
+      subject {project_root_at('test/fixtures/ZergSupport.xcodeproj')}
+      it {should == 'test/fixtures'}
+    end
+
+    context "when given 'test/fixtures'" do
+      subject {project_root_at('test/fixtures')}
+      it {should == 'test'}
+    end
+
+    context "when given 'test'" do
+      subject {project_root_at('test')}
+      it {should == 'test'}
+    end
+
+    context "when given 'test/fixtures/TestApp'" do
+      subject {project_root_at('test/fixtures/TestApp')}
+      it {should == 'test/fixtures/TestApp'}
+    end
+
+    context "when given 'test/fixtures/TestApp/TestApp.xcodeproj'" do
+      subject {project_root_at('test/fixtures/TestApp/TestApp.xcodeproj')}
+      it {should == 'test/fixtures/TestApp'}
+    end
   end
 
 end
