a362b5b47562e788b9179a26deab5fad025c9902
Jason Felice <jason.m.felice@gmail.com>
2012-01-01 15:37:33 -0500

    Fix up archiver spec

--- spec/archiver_spec.rb
+++ spec/archiver_spec.rb
@@ -4,18 +4,9 @@ describe ZergXcode::Archiver do
   Parser = ZergXcode::Parser
   Archiver = ZergXcode::Archiver
   XcodeObject = ZergXcode::XcodeObject
-  
-  before do
-    @sub1 = XcodeObject.new :array => ['a', 'b', 'c'], :string => 's'
-    @sub1.archive_id = '39'
-    @sub2 = XcodeObject.new :hash => { :k => 'v', :k2 => 'v2' }, :sub1 => @sub1
-    @sub2.archive_id = '42'
-    @root = XcodeObject.new :sub1 => @sub1, :sub2 => @sub2
-    @root.archive_id = '49'
-    @root.version = 45
-    
-    @archived = {
-      "archiveVersion" => '1',
+
+  let(:archived_hash) do
+    { "archiveVersion" => '1',
       "rootObject" => '49',
       "classes" => {},
       "objectVersion" => '45',
@@ -25,34 +16,68 @@ describe ZergXcode::Archiver do
         "42" => { :hash => { :k => "v", :k2 => "v2" }, :sub1 => "39" }
       }
     }
-    
-    @pbxdata = File.read 'test/fixtures/project.pbxproj'     
-  end
-  
-  it "archives to a hash" do
-    Archiver.archive_to_hash(@root).should == @archived
-  end
-  
-  it "unarchives a hash" do
-    root = Archiver.unarchive_hash @archived
-    root[:sub1][:s].should == @sub1[:s]
-    root[:sub1][:array].should == @sub1[:array]
-    root[:sub2][:hash].should == @sub2[:hash]
-    root[:sub2][:sub1].should == root[:sub1]
   end
 
-  describe 'unarchiving an archive' do
+  let(:sub1) do
+    sub1 = XcodeObject.new :array => ['a', 'b', 'c'], :string => 's'
+    sub1.archive_id = '39'
+    sub1
+  end
+
+  let(:sub2) do
+    sub2 = XcodeObject.new :hash => { :k => 'v', :k2 => 'v2' }, :sub1 => sub1
+    sub2.archive_id = '42'
+    sub2
+  end
+
+  let(:root) do
+    root = XcodeObject.new :sub1 => sub1, :sub2 => sub2
+    root.archive_id = '49'
+    root.version = 45
+    root
+  end
+  
+  context 'when archiving' do
+    it 'produces the expected hash for a sample object graph' do
+      Archiver.archive_to_hash(root).should == archived_hash
+    end
+  end
+
+  context 'when unarchiving' do
+    let(:pbxdata) {File.read 'test/fixtures/project.pbxproj'}
+  
+    context 'from a hash' do
+      it 'produces the expected object graph' do
+        unarchived_root = Archiver.unarchive_hash archived_hash
+        unarchived_root[:sub1][:s].should == sub1[:s]
+        unarchived_root[:sub1][:array].should == sub1[:array]
+        unarchived_root[:sub2][:hash].should == sub2[:hash]
+        unarchived_root[:sub2][:sub1].should == unarchived_root[:sub1]
+      end
+    end
+
+    context 'from our fixture' do
+      it 'does not raise an error' do
+        lambda {Archiver.unarchive pbxdata}.should_not raise_error
+      end
+
+      it 'produces a project with a valid product path' do
+        project = Archiver.unarchive pbxdata
+        project['targets'][0]['productReference']['path'].should == 'TestApp.app'
+      end
+    end
+
+  end
+
+  describe 'archiving then unarchiving our fixture' do
+    let(:pbxdata) {File.read 'test/fixtures/project.pbxproj'}
+
     it "should produce the same hash" do
-      golden_hash = Parser.parse @pbxdata
-      project = Archiver.unarchive @pbxdata
+      golden_hash = Parser.parse pbxdata
+      project = Archiver.unarchive pbxdata
       dupdata = Archiver.archive project
       Parser.parse(dupdata).should == golden_hash
     end
   end
   
-  it "unarchives our fixture" do
-    project = Archiver.unarchive @pbxdata
-    project['targets'][0]['productReference']['path'].should == 'TestApp.app'
-  end
-
 end
