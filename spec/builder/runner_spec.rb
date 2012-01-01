9c9412df1b06f2d935d72f9d9dcccd9f35c69dd6
Jason Felice <jason.m.felice@gmail.com>
2012-01-01 15:43:17 -0500

    Convert Runner tests to RSpec

---
+++ spec/builder/runner_spec.rb
@@ -0,0 +1,27 @@
+require 'zerg_xcode'
+
+describe ZergXcode::Builder::Runner do
+  before do
+    @project = ZergXcode.load 'test/fixtures/ClosedLib'
+    @configuration = 'Release'
+    @sdk = ZergXcode::Builder::Sdk.all.
+        detect { |s| /i(?:OS|Phone) .*$/ =~ s[:name] }
+    @golden_build_path = 'test/fixtures/ClosedLib/build/Release-iphoneos'
+    @product = @golden_build_path + '/libClosedLib.a'
+    ZergXcode::Builder::Runner.clean(@project, @sdk, @configuration)
+  end
+  
+  after do
+    ZergXcode::Builder::Runner.clean @project, @sdk, @configuration
+  end
+  
+  it "builds and cleans the project" do
+    build_path = ZergXcode::Builder::Runner.build @project, @sdk, @configuration
+    build_path.should be_true
+    build_path.should == @golden_build_path
+    File.exist?(@product).should be_true
+
+    ZergXcode::Builder::Runner.clean(@project, @sdk, @configuration).should be_true
+    File.exist?(@product).should be_false
+  end
+end
