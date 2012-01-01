4e6a8316369f2ff4619e73b6dff2b3e8b8a88e63
Jason Felice <jason.m.felice@gmail.com>
2012-01-01 13:19:24 -0500

    Convert Encoder tests to RSpec

---
+++ spec/encoder_spec.rb
@@ -0,0 +1,17 @@
+require 'zerg_xcode'
+
+describe ZergXcode::Encoder do
+
+  describe 'encoded and reparsed project' do
+
+    let(:fixture_contents) {File.read 'test/fixtures/project.pbxproj'}
+    let(:original_parsed_project) {ZergXcode::Parser.parse(fixture_contents)}
+    let(:reencoded_project) {ZergXcode::Encoder.encode(original_parsed_project)}
+    subject {ZergXcode::Parser.parse(reencoded_project)}
+
+    it 'should be identical to the originally parsed project' do
+      should == original_parsed_project
+    end
+  end
+
+end
