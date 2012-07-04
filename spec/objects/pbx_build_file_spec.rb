282b4e8f1cad06ea16c4125886922e98f6f65750
Jason Felice <jason.m.felice@gmail.com>
2012-07-04 10:26:08 -0400

    Finish conversion of build file tests to RSpec

--- spec/objects/pbx_build_file_spec.rb
+++ spec/objects/pbx_build_file_spec.rb
@@ -4,9 +4,10 @@ describe PBXBuildFile = ZergXcode::Objects::PBXBuildFile do
 
   let(:target) {ZergXcode.load('spec/fixtures/project.pbxproj')['targets'].first}
   let(:sources_phase) {target['buildPhases'][1]}
+  let(:loaded_build_file) {sources_phase['files'].first}
 
   describe 'the first build file in the fixture' do
-    subject {sources_phase['files'].first}
+    subject {loaded_build_file}
 
     it {should be_kind_of(PBXBuildFile)}
 
@@ -20,6 +21,16 @@ describe PBXBuildFile = ZergXcode::Objects::PBXBuildFile do
 
     it 'has an xref_name matching the filename' do
       subject.xref_name.should == subject.filename
+    end
+  end
+
+  describe 'PBXBuildFile for the same fileRef as loaded' do
+    subject {PBXBuildFile.for loaded_build_file['fileRef']}
+
+    it {should be_kind_of(PBXBuildFile)}
+
+    it 'has the same attributes' do
+      subject._attr_hash.should == loaded_build_file._attr_hash
     end
   end
 
