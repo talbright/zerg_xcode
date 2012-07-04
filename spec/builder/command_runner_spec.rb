763910fb2ff2cd322c5d5105a4de8bb5d659e2d8
Jason Felice <jason.m.felice@gmail.com>
2012-07-04 09:27:50 -0400

    CommandRunner responds with command's output

--- spec/builder/command_runner_spec.rb
+++ spec/builder/command_runner_spec.rb
@@ -11,4 +11,9 @@ describe CommandRunner = ZergXcode::Builder::CommandRunner do
     Kernel.should_receive(:`).with('ls / 2>&1').and_return("")
     subject.run(['ls', '/'])
   end
+
+  it "responds with the command's output" do
+    Kernel.should_receive(:`).and_return("foo")
+    subject.run(['ls', '/']).should == 'foo'
+  end
 end
