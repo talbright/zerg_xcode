502e6ab0c75ec9abea380606f5f5d837bf7c3229
Jason Felice <jason.m.felice@gmail.com>
2012-07-04 09:34:55 -0400

    Extract CommandRunner#make_command

--- lib/zerg_xcode/builder/command_runner.rb
+++ lib/zerg_xcode/builder/command_runner.rb
@@ -4,8 +4,13 @@ module ZergXcode::Builder
 
 class CommandRunner
   def run(command_words)
-    Kernel.send(:`, command_words.shelljoin + ' 2>&1')
+    Kernel.send(:`, make_command(command_words))
   end
+
+  def make_command(command_words)
+    command_words.shelljoin + ' 2>&1'
+  end
+  private :make_command
 end
 
 end
