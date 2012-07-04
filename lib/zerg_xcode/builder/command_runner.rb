eb5d110a69ef1da3b5e32ac67e136a590c9e7ce8
Jason Felice <jason.m.felice@gmail.com>
2012-07-04 09:26:04 -0400

    CommandRunner runs the command with an error redirection

--- lib/zerg_xcode/builder/command_runner.rb
+++ lib/zerg_xcode/builder/command_runner.rb
@@ -3,6 +3,7 @@ module ZergXcode::Builder
 
 class CommandRunner
   def run(command_words)
+    Kernel.`(command_words.join(' ') + ' 2>&1')
   end
 end
 
--- spec/builder/command_runner_spec.rb
+++ spec/builder/command_runner_spec.rb
@@ -2,7 +2,13 @@ require 'zerg_xcode'
 
 describe CommandRunner = ZergXcode::Builder::CommandRunner do
   it {should respond_to(:run)}
+
   it 'accepts an array of shell words' do
+    subject.run(['ls', '/'])
+  end
+
+  it 'runs the command using ` and captures the error stream' do
+    Kernel.should_receive(:`).with('ls / 2>&1').and_return("")
     subject.run(['ls', '/'])
   end
 end
