befc945ca31b136567aa01ce154ef5c6ae4de32a
Jason Felice <jason.m.felice@gmail.com>
2012-07-03 21:15:42 -0400

    Extract custom matcher for options with values

--- spec/builder/build_command_maker_spec.rb
+++ spec/builder/build_command_maker_spec.rb
@@ -1,18 +1,25 @@
 require 'zerg_xcode'
 
+RSpec::Matchers.define :have_argument_with_value do |argument, value|
+  match do |actual|
+    i = subject.index(argument)
+    subject[i+1] == value
+  end
+  description do
+    "have an argument '#{argument}' with value '#{value}'"
+  end
+end
+
 describe BuildCommandMaker = ZergXcode::Builder::BuildCommandMaker do
   
-  describe "its result" do
+  describe "the resulting command" do
     let(:sdk) {ZergXcode::Builder::Sdk.new 'iPhone SDKs', 'iOS 5.1', 'iphone5.1'}
     subject {BuildCommandMaker.new(sdk).make}
 
     it {should be_kind_of(Array)}
     it {should include('xcodebuild')}
     it {should include('-alltargets')}
-    it 'passes the sdk' do
-      i = subject.index('-sdk')
-      subject[i+1].should == 'iphone5.1'
-    end
+    it {should have_argument_with_value('-sdk', 'iphone5.1')}
   end
 
 end
